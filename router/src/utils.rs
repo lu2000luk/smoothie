3547a6e64152a6d720fe241329a35d2d8c35c9a6
lu2000luk <81090607+lu2000luk@users.noreply.github.com>
2026-07-24 17:16:32 +0200

    beautiful schema

--- router/src/utils.rs
+++ router/src/utils.rs
@@ -1,20 +1,33 @@
-use rand::{distr::{Distribution, weighted::WeightedIndex}, seq::index};
+use rand::{
+    distr::{Distribution, weighted::WeightedIndex},
+    seq::index,
+};
 
 use crate::ServerConfig;
+
+// server ranker system (to be done)
+// based on scores
+// each server has a score of 100
+// each request the server served decrease score by 1
+// if the server served a request from this host in last 24hrs boost score by 500 (means app is cached in this server)
+// pick highest score server to serve the request
 
 pub fn get_server(servers: &Vec<ServerConfig>) -> Option<ServerConfig> {
     if servers.is_empty() {
         return None;
     }
 
-     let ids = servers.iter().map(|s| s.id.clone()).collect::<Vec<String>>();
-     let powers = servers.iter().map(|s| s.power).collect::<Vec<u32>>();
+    let ids = servers
+        .iter()
+        .map(|s| s.id.clone())
+        .collect::<Vec<String>>();
+    let powers = servers.iter().map(|s| s.power).collect::<Vec<u32>>();
 
     let dist = WeightedIndex::new(&powers).ok()?;
     let mut rng = rand::rng();
     let index = dist.sample(&mut rng);
 
     let server = servers.get(index).cloned();
-    
-server
+
+    server
 }
